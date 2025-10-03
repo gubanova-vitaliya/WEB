03026aa17b91883801e48b1f54471b7e02f547ed
guban <vitaliagubanova@gmail.com>
2025-10-03 05:05:22 +0300

    новый журнал расчетов

--- internal/app/handler/gas.go
+++ internal/app/handler/gas.go
@@ -1,8 +1,10 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
+	"time"
 
 	"WEB/internal/app/ds"
 
@@ -11,16 +13,7 @@ import (
 )
 
 func (h *Handler) GetAllCalculations(ctx *gin.Context) {
-	var calculations []ds.Calculation
-	var err error
-
-	search := ctx.Query("search")
-	if search == "" {
-		calculations, err = h.Repository.GetAllCalculations()
-	} else {
-		calculations, err = h.Repository.SearchCalculationsByName(search)
-	}
-
+	calculations, err := h.Repository.GetAllCalculations()
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{
 			"error": err.Error(),
@@ -29,10 +22,9 @@ func (h *Handler) GetAllCalculations(ctx *gin.Context) {
 		return
 	}
 
-	ctx.HTML(http.StatusOK, "calculations.html", gin.H{ // Используем отдельный шаблон
+	ctx.HTML(http.StatusOK, "calculations.html", gin.H{
 		"data":          calculations,
-		"journal_count": h.Repository.GetJournalCountDefault(), // Используем новую функцию
-		"search":        search,
+		"journal_count": h.Repository.GetJournalCountDefault(),
 	})
 }
 
@@ -62,18 +54,41 @@ func (h *Handler) GetCalculationById(ctx *gin.Context) {
 }
 
 func (h *Handler) CreateCalculation(ctx *gin.Context) {
-	var calculation ds.Calculation
-
-	if err := ctx.ShouldBind(&calculation); err != nil {
-		h.errorHandler(ctx, http.StatusBadRequest, err)
+	// Получаем gas_id из формы
+	gasIDStr := ctx.PostForm("gas_id")
+	gasID, err := strconv.Atoi(gasIDStr)
+	if err != nil {
+		h.errorHandler(ctx, http.StatusBadRequest, fmt.Errorf("invalid gas ID"))
 		return
 	}
 
-	// Логика расчета давления по формуле идеального газа
-	// P₂ = P₁ * T₂ / T₁ (температуры в Кельвинах)
-	// Добавьте здесь расчет, если нужно
+	// Создаем новый расчет с пустыми полями для ввода
+	calculation := ds.Calculation{
+		UserID:       1, // Временно используем ID пользователя 1
+		GasID:        uint(gasID),
+		Volume:       0.0, // Пустое поле для ввода
+		Temperature1: 0.0, // Пустое поле для ввода
+		Temperature2: 0.0, // Пустое поле для ввода
+		Pressure1:    0.0, // Пустое поле для ввода
+		Pressure2:    0.0, // Будет рассчитано после ввода данных
+		Mass:         0.0, // Пустое поле для ввода
+		Moles:        0.0, // Пустое поле для ввода
+		DateCreate:   time.Now(),
+		DateUpdate:   time.Now(),
+		IsDeleted:    false,
+	}
 
-	err := h.Repository.CreateCalculation(&calculation)
+	err = h.Repository.CreateCalculation(&calculation)
+	if err != nil {
+		h.errorHandler(ctx, http.StatusInternalServerError, err)
+		return
+	}
+
+	ctx.Redirect(http.StatusSeeOther, "/calculations")
+}
+
+func (h *Handler) ClearAllCalculations(ctx *gin.Context) {
+	err := h.Repository.ClearAllCalculations()
 	if err != nil {
 		h.errorHandler(ctx, http.StatusInternalServerError, err)
 		return
@@ -83,14 +98,32 @@ func (h *Handler) CreateCalculation(ctx *gin.Context) {
 }
 
 func (h *Handler) GetAllGases(ctx *gin.Context) {
-	gases, err := h.Repository.GetAllGases()
+	var gases []ds.Gas
+	var err error
+
+	query := ctx.Query("query")
+	if query == "" {
+		// Если поисковый запрос пустой, показываем все газы
+		gases, err = h.Repository.GetAllGases()
+	} else {
+		// Если есть поисковый запрос, ищем по нему
+		gases, err = h.Repository.SearchGases(query)
+
+		// Если найден ровно один газ, перенаправляем на его детальную страницу
+		if err == nil && len(gases) == 1 {
+			logrus.Printf("Found exactly one gas for query '%s', redirecting to gas detail page", query)
+			ctx.Redirect(http.StatusFound, fmt.Sprintf("/gases/%d", gases[0].ID))
+			return
+		}
+	}
+
 	if err != nil {
 		h.errorHandler(ctx, http.StatusInternalServerError, err)
 		return
 	}
 
 	// Отладочный вывод
-	logrus.Printf("Found %d gases:", len(gases))
+	logrus.Printf("Found %d gases (query: '%s'):", len(gases), query)
 	for i, gas := range gases {
 		logrus.Printf("Gas %d: %+v", i, gas)
 	}
@@ -98,9 +131,72 @@ func (h *Handler) GetAllGases(ctx *gin.Context) {
 	// Передаем данные в шаблон
 	ctx.HTML(http.StatusOK, "index.html", gin.H{
 		"gases":        gases,
-		"journalCount": h.Repository.GetJournalCountDefault(), // Используем новую функцию
-		"query":        ctx.Query("query"),
+		"journalCount": h.Repository.GetJournalCountDefault(),
+		"query":        query,
 	})
+}
+
+// GetGasById обработчик для получения детальной информации о газе
+func (h *Handler) GetGasById(ctx *gin.Context) {
+	strId := ctx.Param("id")
+	id, err := strconv.Atoi(strId)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid gas ID",
+		})
+		logrus.Error(err)
+		return
+	}
+
+	gas, err := h.Repository.GetGasByID(id)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		logrus.Error(err)
+		return
+	}
+
+	if gas == nil {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"error": "Gas not found",
+		})
+		return
+	}
+
+	ctx.HTML(http.StatusOK, "gas_detail.html", gin.H{
+		"gas":          gas,
+		"journalCount": h.Repository.GetJournalCountDefault(),
+	})
+}
+
+// DeleteCalculation обработчик для удаления расчета
+func (h *Handler) DeleteCalculation(ctx *gin.Context) {
+	// Поддержка как DELETE запросов, так и POST с _method=DELETE
+	method := ctx.PostForm("_method")
+	if method == "DELETE" || ctx.Request.Method == "DELETE" {
+		strId := ctx.Param("id")
+		id, err := strconv.Atoi(strId)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": "Invalid calculation ID",
+			})
+			logrus.Error(err)
+			return
+		}
+
+		err = h.Repository.DeleteCalculation(id)
+		if err != nil {
+			h.errorHandler(ctx, http.StatusInternalServerError, err)
+			return
+		}
+
+		ctx.Redirect(http.StatusSeeOther, "/calculations")
+	} else {
+		ctx.JSON(http.StatusMethodNotAllowed, gin.H{
+			"error": "Method not allowed",
+		})
+	}
 }
 
 // Добавьте обработчик для главной страницы
--- internal/app/repository/gas.go
+++ internal/app/repository/gas.go
@@ -51,9 +51,47 @@ func (r *Repository) CreateCalculation(calculation *ds.Calculation) error {
 	return r.db.Create(calculation).Error
 }
 
+func (r *Repository) DeleteCalculation(id int) error {
+	return r.db.Delete(&ds.Calculation{}, id).Error
+}
+
+func (r *Repository) ClearAllCalculations() error {
+	// Сначала очищаем связанную таблицу calculation_gas
+	err := r.db.Where("1 = 1").Delete(&ds.CalculationGas{}).Error
+	if err != nil {
+		return err
+	}
+
+	// Затем очищаем таблицу calculations
+	return r.db.Where("1 = 1").Delete(&ds.Calculation{}).Error
+}
+
 func (r *Repository) GetAllGases() ([]ds.Gas, error) {
 	var gases []ds.Gas
 	err := r.db.Where("is_deleted = ?", false).Find(&gases).Error
+	if err != nil {
+		return nil, err
+	}
+	return gases, nil
+}
+
+func (r *Repository) GetGasByID(id int) (*ds.Gas, error) {
+	var gas ds.Gas
+	err := r.db.Where("id = ? AND is_deleted = ?", id, false).First(&gas).Error
+
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &gas, nil
+}
+
+func (r *Repository) SearchGases(query string) ([]ds.Gas, error) {
+	var gases []ds.Gas
+	err := r.db.Where("(title ILIKE ? OR formula ILIKE ? OR description ILIKE ?) AND is_deleted = ?",
+		"%"+query+"%", "%"+query+"%", "%"+query+"%", false).Find(&gases).Error
 	if err != nil {
 		return nil, err
 	}
